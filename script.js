9f7e5090d911e0ff9e23631f65d7ca0b26199b1c
imanyousefi13671988-eng <imanyousefi13671988@gmail.com>
2026-07-24 23:33:06 +0330

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ if (envelope) {
 
         music.addEventListener("loadedmetadata", function () {
             music.currentTime = 4;
-            music.currentTimemusic.play();
+                music.play();
         }, { once: true });
 
         if (music.readyState >= 1) {
